9308cb89262eee307c6eb49bf36928dab7278125
maduoma <18477889+maduoma@users.noreply.github.com>
2025-04-26 11:26:07 -0700

    Updated

--- tests/test_rag_chain.py
+++ tests/test_rag_chain.py
@@ -1,9 +1,7 @@
 # tests/test_rag_chain.py
-import pytest
+
 from unittest.mock import patch
 from app.llm.rag_chain import RAGPipeline
-
-
 
 @patch("app.llm.generator._call_claude", return_value="Mocked answer")
 @patch("app.llm.generator.openai.ChatCompletion.create")
@@ -14,10 +12,3 @@ def test_rag_pipeline(mock_create):
     # ----- Act ---------------------------------------------------------------
     result = rag.run("What is AI?")
     assert "Mocked answer" in result
-# from app.llm.rag_chain import RAGPipeline
-
-# def test_rag_pipeline():
-#     rag = RAGPipeline()
-#     result = rag.run("What is AI?")
-#     assert isinstance(result, str)
-#     assert "AI" in result or "context" in result
